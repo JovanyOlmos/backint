7360d0dcb0abcb91fdba120075145a28f79c811c
jolmos <jj.olmoszavala@ugto.mx>
2021-11-25 16:20:50 -0600

    Controlador de BD

--- core/DBObj.php
+++ core/DBObj.php
@@ -22,18 +22,10 @@ class DBObj {
     private $num_records = 0;
 
     /**
-     * Connect to DB. Initialize a MySQLi Object
+     * Constructor
      */
     public function __construct() {
-        try {
-            $this->connection = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME);
-        } catch (Exception  $th) {
-            $err = new ErrObj("Fatal error on server. ".$th->getMessage()
-                ." Linea: ".$th->getLine()
-                ." Archivo: ".$th->getFile(), INTERNAL_SERVER_ERROR);
-            $err->sendError();
-            die();
-        }
+        
     }
 
     /**
@@ -44,6 +36,7 @@ class DBObj {
      * @return mysqli_result
      */
     public function getFetchAssoc($query) {
+        $this->initConn();
         try {
             if($result = mysqli_query($this->connection, $query)) {
                 $this->num_records = $result->num_rows;
@@ -72,6 +65,7 @@ class DBObj {
      * @return ErrObj
      */
     public function doQuery($query): ErrObj {
+        $this->initConn();
         if($result = mysqli_query($this->connection, $query)) {
             mysqli_close($this->connection);
             return new ErrObj("", CREATED);
@@ -90,6 +84,21 @@ class DBObj {
      */
     public function getNumRecords() {
         return $this->num_records;
+    }
+
+    /**
+     * Connect to DB. Initialize a MySQLi Object
+     */
+    private function initConn() {
+        try {
+            $this->connection = new mysqli(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME);
+        } catch (Exception  $th) {
+            $err = new ErrObj("Fatal error on server. ".$th->getMessage()
+                ." Linea: ".$th->getLine()
+                ." Archivo: ".$th->getFile(), INTERNAL_SERVER_ERROR);
+            $err->sendError();
+            die();
+        }
     }
 }
 ?>
